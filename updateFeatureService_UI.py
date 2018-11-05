5caa3da9c773f03ee2b87bd7531ff4491760eae6
Darius Mirdamadi <darius.mirdamadi@carnellgroup.co.uk>
2018-11-05 15:43:17 +0000

    Hello

--- updateFeatureService_UI.py
+++ updateFeatureService_UI.py
@@ -8,6 +8,8 @@ import updatewebmap
 import datetime
 import create_flow_direction
 import import_flood_events
+print ("Hello")
+
 
 # Create log file
 dirname = os.path.dirname(os.path.realpath(__file__))
